318408ea083976b077603fb3e658bfcd6a525d1b
beader <beader.chen@gmail.com>
2019-08-20 13:36:55 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 cd code
 
-python task1.py infer --test_data_file /tcdata/test.json --test_table_file /tcdata/test.tables.json --model_weights ../model/task1.12-0.852.h5 --batch_size 256 --output_file ../task1_output.json
-python task2.py infer --test_data_file /tcdata/test.json --test_table_file /tcdata/test.tables.json --model_weights ../model/task2.h5 --batch_size 256 --synthesis_with_task1_output 1 --task1_output ../task1_output.json --submit_output ../result.json
+python task1.py infer --test_data_file /tcdata/final_test.json --test_table_file /tcdata/final_test.tables.json --model_weights ../model/task1.12-0.852.h5 --batch_size 256 --output_file ../task1_output.json
+python task2.py infer --test_data_file /tcdata/final_test.json --test_table_file /tcdata/final_test.tables.json --model_weights ../model/task2.h5 --batch_size 256 --synthesis_with_task1_output 1 --task1_output ../task1_output.json --submit_output ../result.json

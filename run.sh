7a186337b28c8810a4fcbf0e973fe7a3235435a0
beader <beader.chen@gmail.com>
2019-10-14 15:48:19 +0800

    clean master branch

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd code
-
-python task1.py infer --test_data_file /tcdata/final_test.json --test_table_file /tcdata/final_test.tables.json --model_weights ../model/task1.h5 --batch_size 256 --output_file ../task1_output.json
-python task2.py infer --test_data_file /tcdata/final_test.json --test_table_file /tcdata/final_test.tables.json --model_weights ../model/task2.h5 --batch_size 256 --synthesis_with_task1_output 1 --task1_output ../task1_output.json --submit_output ../result.json
